516c7c1d0ea6831f5261ebc6b9de1ee4c0a2973d
sajal <sajalsahu8@gmail.com>
2023-07-29 02:12:39 +0400

    inputoutput

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    //input variable
+    cin>>a;
+
+    //output variable
+    cout<<a;
+    return 0;
+}
